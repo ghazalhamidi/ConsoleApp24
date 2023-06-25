078743dee922b19b4804f4fe1fd2ee70ac655136
Partiran <Partiran@DESKTOP-IR2LBIU>
2023-06-25 19:30:06 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class circle
+    {
+        private double radius;
+        public circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double masahat()
+        {
+            return Math.PI * radius * radius;
+        }
+        public double mohit()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            double radius;
+            radius = Convert.ToDouble(Console.ReadLine());
+            circle c = new circle(radius);
+            Console.WriteLine("radius:" + radius);
+            Console.WriteLine("masahat:" + c.masahat());
+
+            Console.WriteLine("circumference:" + c.mohit());
+        }
+    }
+}
